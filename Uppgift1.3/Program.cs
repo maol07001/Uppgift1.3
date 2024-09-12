c2378289f8134307f407a20f15dee04850733d18
maol07001 <maol07001@E-PW05CGR4>
2024-09-12 14:35:39 +0200

    Add project files.

---
+++ Uppgift1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
